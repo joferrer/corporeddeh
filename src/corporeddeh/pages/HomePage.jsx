d60be056405fb51730d595d61b3560095b2885ac
Jeison Ferrer <jeison120120@hotmail.com>
2023-11-13 17:01:31 -0500

    feat: carousel TODO: 1. Aliniar el texto para que se ubique abajo

--- src/corporeddeh/pages/HomePage.jsx
+++ src/corporeddeh/pages/HomePage.jsx
@@ -1,7 +1,9 @@
-import { Card, Grid, Typography } from "@mui/material";
+import { Box, Button, Card, Grid, Link, Paper, Typography } from "@mui/material";
 import Layout from "./layout/Layout";
 import { CalendarMonth, DescriptionOutlined, EventAvailable, Place } from "@mui/icons-material";
 import { routes } from "../routes/routes";
+import Carousel from "react-material-ui-carousel";
+
 
 const NavegationCard = ({ children, color, link = "/" }) => {
   return <Grid item xs={6}
@@ -21,6 +23,7 @@ const NavegationCard = ({ children, color, link = "/" }) => {
  * @returns Navegation component
  */
 const NavegationComponent = () => {
+  //TODO Falta el fondo de la img
   return <Grid sx={{ padding: 1 }}>
     <Typography>Corporación Red Departamental de Defensores de DDHHH</Typography>
     <Grid container spacing={1} sx={{ marginTop: 0 }}>
@@ -46,13 +49,98 @@ const NavegationComponent = () => {
   </Grid>
 }
 
+
+const post = {
+  image: "https://pbs.twimg.com/media/FCVslvrXoAAIzS7.jpg:large",
+  imageText:"Friendly noodels",
+  title: "Noodels",
+  description: "A cute cat",
+  linkText:"/noodels"
+}
+const PrincipalImgComponent = ()=>{
+  return <Grid>
+    <Example />
+  </Grid>
+}
+
+function Example(props)
+{
+    var items = [
+        {
+            name: "Random Name #1",
+            description: "Gira territorial de paz, escenarios de diálogo con organizaciones sociales, liderazgos comunales, campesinos, rurales y procesos de ciudad.",
+            img: "https://pbs.twimg.com/media/FCVslvrXoAAIzS7.jpg:large"
+        },
+        {
+            name: "Random Name #2",
+            description: "Hello World!",
+            img:"https://pbs.twimg.com/media/FCVslvrXoAAIzS7.jpg:large"
+        }
+    ]
+
+    return (
+        <Carousel>
+            {
+                items.map( (item, i) => <Item key={i} item={item} /> )
+            }
+        </Carousel>
+    )
+}
+
+function Item(props)
+{
+    return (
+      <Paper
+      sx={{
+        position: 'relative',
+        backgroundColor: 'grey.800',
+        color: '#fff',
+        mb: 4,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        backgroundImage: `url(${props.item.img})`,
+        
+      }}
+    >
+      {/* Increase the priority of the hero background image */}
+      {<img style={{ display: 'none',  }} src={props.item.img} alt={post.imageText} />}
+      <Box
+        sx={{
+          position: 'absolute',
+          top: 0,
+          bottom: 0,
+          right: 0,
+          left: 0,
+          backgroundColor: 'rgba(0,0,0,.3)',
+        }}
+      />
+      <Grid container >
+        <Grid item md={6}
+        style={{textAlign:"left", minHeight:"180px", justifyContent:"flex-end"}}>
+          <Box
+            sx={{
+              position: 'relative',
+              p: { xs: 3,},
+              pr: { md: 0 },
+            }}
+          >
+   
+            <Typography variant="body1" color="inherit" >
+              {props.item.description}
+            </Typography>
+          </Box>
+        </Grid>
+      </Grid>
+    </Paper>
+    )
+}
+
 export const HomePage = () => {
   return <Layout>
     <Grid>
-      //Img
-      <Grid>
-
-      </Grid>
+      
+      <PrincipalImgComponent />
 
       <NavegationComponent />
 
