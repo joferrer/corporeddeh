60440d5be87bd7dd298fa311c36eeb595658f36b
Jeison Ferrer <jeison120120@hotmail.com>
2023-12-28 19:51:32 -0500

    auth slice y thunks ademas del metodo de login, falta conectar y provar

---
+++ src/backend/firebase/AuthFirebaseProvider.js
@@ -0,0 +1,34 @@
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { FireBaseAuth } from "./firebaseConfig";
+
+const googleProvider = new GoogleAuthProvider();
+
+export const singInWithGoogle = async () => {
+    try {
+      const result = await signInWithPopup(FireBaseAuth, googleProvider);
+      const { displayName, email, photoURL, uid } = result.user;
+        
+      return {
+        ok: true,
+        //userInfo
+        displayName,
+        email,
+        photoURL,
+        uid,
+      };
+    } catch (error) {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+  
+      return {
+        ok: false,
+        errorCode,
+        errorMessage,
+      };
+    }
+}
+  
+
+export const logoutFireBase = async () => {
+  return await FireBaseAuth.signOut();
+};
---
+++ src/backend/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit"
+import { AuthSlice } from "./auth"
+
+
+export const store = configureStore({
+    reducer: {
+        auth: AuthSlice.reducer,
+    }
+    
+})
